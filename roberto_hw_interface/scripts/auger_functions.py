e0ffedb4106d6223e9141970e45faa64fa18880a
Gabe Wozniak <gfwozniak@gmail.com>
2022-04-28 22:16:15 -0500

    prototyped functions for auger

---
+++ roberto_hw_interface/scripts/auger_functions.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from auger_motor_api import AugerAPI
+
+class AugerFunctions:
+
+    def __init__(self):
+        self.augerapi = AugerAPI()
+
+    def returnToNeutral(self):
+        self.augerapi.setBScrewPosition(position=-5000)
+        self.augerapi.setActuatorPosition(position=92)
+
+    def deployAuger(self):
+        if (self.augerapi.bscrew_position > 10000 or self.augerapi.bscrew_position < -10000):
+            pass
+        self.augerapi.setActuatorPosition(position=3)
+        self.augerapi.setBScrewPositionAndAugerVelocity(position=-7000000, velocity=1)
+        self.returnToNeutral()
+
+    def depositAuger(self):
+        if (self.augerapi.bscrew_position > 10000 or self.augerapi.bscrew_position < -10000):
+            pass
+        self.augerapi.setActuatorPosition(position=100)
+        self.augerapi.setBScrewPosition(position=-2000000)
+        self.augerapi.setAugerVelocityForDuration(velocity=-1, seconds=6)
+        self.returnToNeutral()
+
+
+
+
+        
+#if __name__ == '__main__':
+#    try:
+#        rospy.init_node('auger_motor_calls', anonymous=True)
+#        robot = AugerAPI()
+#        print("Setting actuator position to 100")
+#        robot.setActuatorPosition(position=100)
+#        print("finished")
+#    except rospy.ROSInterruptException:
+#        pass
